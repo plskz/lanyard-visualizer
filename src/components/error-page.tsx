7cca1be626fa96b624d923d0838dedd7f39709a9
Zai Santillan <zaisantillan14@gmail.com>
2024-02-22 11:13:02 +0800

    feat(ErrorPage): add error message

--- src/components/error-page.tsx
+++ src/components/error-page.tsx
@@ -2,16 +2,29 @@ import Link from 'next/link'
 
 export default function ErrorPage() {
   return (
-    <div className='min-h-screen grid place-items-center'>
-      <div className='flex flex-col items-center gap-5'>
-        <p className='text-6xl font-bold text-white'>User not found</p>
+    <main className='place-items-center grid min-h-screen'>
+      <div className='flex flex-col items-center gap-5 w-11/12 max-w-[611px] text-center'>
+        <h1 className='text-5xl font-bold text-white'>User not found</h1>
+        <p className='text-white/50'>
+          Make sure you entered a valid Discord user ID and make sure the user
+          is in{' '}
+          <Link
+            href='https://lanyard.rest/discord'
+            className='underline-dashed underline-white/20 underline'
+            target='_blank'
+          >
+            Lanyard&apos;s Discord server
+          </Link>
+          . Reload the page after you join the Discord server or try with an
+          user ID who is already in Discord.
+        </p>
         <Link
-          className='w-32 px-4 py-2 text-sm text-center rounded-lg bg-gray-500/5 hover:bg-gray-500/20 hover:text-white/90 text-white/60'
+          className='bg-gray-500/5 hover:bg-gray-500/20 hover:text-white/90 text-white/60 w-32 px-4 py-2 text-sm text-center rounded-lg'
           href='/'
         >
           Return Home
         </Link>
       </div>
-    </div>
+    </main>
   )
 }
