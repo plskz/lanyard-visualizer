20a66396412cfaadeb20ace2a72be7cf41e5d74e
Zai Santillan <zaisantillan14@gmail.com>
2024-02-22 11:24:14 +0800

    feat: add vercel speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
-import { Analytics } from '@vercel/analytics/react'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+
+import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -20,6 +22,7 @@ export default function RootLayout({
       <body className={`${inter.className} antialiased`}>
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   )
