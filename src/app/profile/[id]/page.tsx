b6edeb2a8a4bd05b46e9ad6c12dafbd0e079f688
Zai Santillan <zaisantillan14@gmail.com>
2024-03-13 23:41:55 +0800

    feat(profile): add return home

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -2,26 +2,34 @@ import Activities from '@/components/activities'
 import Details from '@/components/details'
 import SocialLinks from '@/components/social-links'
 import { Icon } from '@/components/ui/evervault-card'
+import Link from 'next/link'
 
 export default function Page({ params }: { params: { id: string } }) {
   return (
-    <div className='min-h-screen place-items-center grid'>
+    <div className='flex flex-col items-center justify-center min-h-screen gap-5'>
       <div className='border border-white/[0.2] flex flex-col items-start w-4/5 max-w-sm p-4 relative'>
-        <Icon className='absolute size-6 -top-3 -left-3' />
-        <Icon className='absolute size-6 -bottom-3 -left-3' />
-        <Icon className='absolute size-6 -top-3 -right-3' />
-        <Icon className='absolute size-6 -bottom-3 -right-3' />
+        <Icon className='size-6 -top-3 -left-3 absolute' />
+        <Icon className='size-6 -bottom-3 -left-3 absolute' />
+        <Icon className='size-6 -top-3 -right-3 absolute' />
+        <Icon className='size-6 -bottom-3 -right-3 absolute' />
 
-        <div className='flex overflow-x-scroll snap-x snap-mandatory -bg-red-500 size-full'>
+        <div className='snap-x snap-mandatory size-full flex overflow-x-scroll'>
           <div className='size-full shrink-0 snap-center'>
             <Details userID={params.id} />
             <SocialLinks userID={params.id} />
           </div>
-          <div className='w-full shrink-0 snap-center'>
+          <div className='shrink-0 snap-center w-full'>
             <Activities userID={params.id} />
           </div>
         </div>
       </div>
+
+      <Link
+        className='bg-gray-500/5 hover:bg-gray-500/20 hover:text-white/90 text-white/60 w-32 px-4 py-2 text-sm text-center rounded-lg'
+        href='/'
+      >
+        Return Home
+      </Link>
     </div>
   )
 }
